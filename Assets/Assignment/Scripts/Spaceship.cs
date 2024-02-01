af560f4ae658ba4db73b2a886d45d7788ceffbf2
jadonpadon <salvador.jaden@gmail.com>
2024-01-31 23:58:32 -0500

    Assignment 1: Boost uses OnTriggerEnter2D to increase speed spaceship input is back to using steering to incorporate AddTorque and AddForce transform functions used in asteroids

--- Assets/Assignment/Scripts/Spaceship.cs
+++ Assets/Assignment/Scripts/Spaceship.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,10 @@ public class Spaceship : MonoBehaviour
 {
     Rigidbody2D rigidbody;
     public float speed = 5f;
+    float steering;
+    float accelerate;
+    public float steerSpeed = 10f;
+    public float maxSpeed = 10;
     
     // Start is called before the first frame update
     void Start()
@@ -16,13 +21,19 @@ public class Spaceship : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        accelerate = Input.GetAxis("Vertical");
+        steering = Input.GetAxis("Horizontal");
     }
 
     private void FixedUpdate()
     {
-        float moveHor = Input.GetAxis("Horizontal");
-        float moveVer = Input.GetAxis("Vertical");
-        transform.Translate(moveHor * speed * Time.deltaTime, moveVer * speed * Time.deltaTime, 0);
+        rigidbody.AddTorque(steering * -steerSpeed * Time.deltaTime);
+        Vector2 force = transform.up * accelerate * speed * Time.deltaTime;
+        if (rigidbody.velocity.magnitude < maxSpeed);
+        {
+            rigidbody.AddForce(force);
+        }
     }
+
+    
 }
